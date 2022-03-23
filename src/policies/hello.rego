718ce72815da0c4f9499d2fe5c07bb8a618f4bf2
Gert Drapers <gert.drapers@live.com>
2022-03-22 18:00:10 -0700

    update

--- src/policies/hello.rego
+++ src/policies/hello.rego
@@ -4,13 +4,12 @@ package policies.hello
 # only grant access when explicitly granted
 
 default allowed = false
-version = 1
 
 allowed {
     input.role == "web-admin"
 }
 
 allowed {
-    input.version == version
+    input.version == 2
 }
 
--- update.sh
+++ update.sh
@@ -2,8 +2,14 @@ git add .
 git commit -am "update"
 tag=$(svu --strip-prefix)
 git tag ${tag}
+
 policy build src -t opcr.io/gertd/empty:${tag}
+
 policy push opcr.io/gertd/empty:${tag}
+
+policy tag opcr.io/gertd/empty:${tag}  opcr.io/gertd/empty:0.0
 policy push opcr.io/gertd/empty:0.0
+
+policy tag opcr.io/gertd/empty:${tag}  opcr.io/gertd/empty:0
 policy push opcr.io/gertd/empty:0
 
