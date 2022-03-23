de2076bebe2b62248d6b505f247b1c4e84ef3a45
Gert Drapers <gert.drapers@live.com>
2022-03-22 17:53:18 -0700

    update

--- update.sh
+++ update.sh
@@ -4,4 +4,6 @@ tag=$(svu --strip-prefix)
 git tag ${tag}
 policy build src -t opcr.io/gertd/empty:${tag}
 policy push opcr.io/gertd/empty:${tag}
+policy push opcr.io/gertd/empty:0.0
+policy push opcr.io/gertd/empty:0
 
